7860546a364eed9b10410bf6244e0a44a8417990
Guglis02 <gustavomf02@gmail.com>
2021-06-09 12:53:33 -0300

    atividades realizadas

---
+++ haskell06.hs
@@ -0,0 +1,41 @@
+ends :: [Int] -> [Int]
+ends (x:xs) = x:last xs:[]
+
+deduzame :: [Integer] -> [Integer]
+deduzame [] = []
+deduzame (x:xs) = (2 * x) : deduzame xs
+
+deduzame2 :: [Integer] -> [Integer]
+deduzame2 [] = []
+deduzame2 (x:xs) = if (x) > 2
+    then x : deduzame2 xs
+    else deduzame2 xs
+
+geraTabela :: Int -> [(Int,Int)]
+geraTabela 0 = []
+geraTabela n = (n,n^2) : geraTabela (n-1)
+
+contido :: Char -> String -> Bool
+contido c [] = False
+contido c (x:xs) = if (c == x) 
+    then True
+    else contido c xs
+
+translate :: [(Float,Float)] -> [(Float,Float)]
+translate [] = []
+translate ((x,y):xs) = (x + 2, y + 2) : translate xs
+
+countShorts :: [String] -> Int
+countShorts strl = length [str | str <- strl, length str < 5]
+
+countLongs :: [String] -> Int
+countLongs [] = 0
+countLongs (x:xs) = if length x > 5
+    then 1 + countLongs xs
+    else 0 + countLongs xs  
+
+onlyLongs :: [String] -> [String]
+onlyLongs [] = []
+onlyLongs (x:xs) = if length x > 5
+    then x : onlyLongs xs
+    else onlyLongs xs  
